73dd6b67d607b5ce277a18822ac24503f87d532b
Ryan Harwick <ryandharwick@gmail.com>
2022-09-02 16:07:50 -0400

    First program

---
+++ Practice_1.py
@@ -0,0 +1,8 @@
+# Python Programming
+# Practice Program 1
+
+Rad = input("Please enter in the radius of the circle to calculate the circumference: ")
+
+Circum = 2 * float(Rad) * 3.14
+
+print(f"The circumference of a circle with a radius of ", Rad, " is ", Circum, ".")
